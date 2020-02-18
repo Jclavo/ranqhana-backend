6204a99ad808f27c4ce451d0ba90026368178b10
Jclavo <jclavotafur@gmail.com>
2020-02-18 19:33:15 -0300

    add store seeder to the main seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call([
             CountriesTableSeeder::class,
+            StoresTableSeeder::class,
+            
             UsersTableSeeder::class,
            // CommentsTableSeeder::class,
         ]);
