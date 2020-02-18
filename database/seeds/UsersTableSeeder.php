c7af4dc0676601b969539aa3cc46c4a2809ebfc3
Jclavo <jclavotafur@gmail.com>
2020-02-18 19:32:58 -0300

    change the seeder using factory

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -11,14 +13,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
+        //     'name' => 'Jose Clavo',
+        //     'email' => 'jc@gmail.com',
+        //     'password' => bcrypt('123'),
+        //     'identification' => '10210210210',
+        //     'country_code' => 55,
+        // ]);
+
+        factory(User::class)->create([
             'name' => 'Jose Clavo',
             'email' => 'jc@gmail.com',
             'password' => bcrypt('123'),
             'identification' => '10210210210',
             'country_code' => 55,
+            'store_id' => 1,
         ]);
-
-        // factory(User::class)->create(['password' => '']);
     }
 }
