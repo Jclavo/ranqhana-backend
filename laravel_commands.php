6f7246d1fa8cd13e29f688c5945c7254eba3fb54
Jclavo <jclavotafur@gmail.com>
2020-02-20 21:38:40 -0300

    basic laravels commands

---
+++ laravel_commands.php
@@ -0,0 +1,37 @@
+LARAVEL Basic commands
+
+- CREATE MODEL (& migration)
+
+php artisan make:model Flight -m
+
+- CREATE MIGRATION
+
+php artisan make:migration create_users_table --create=users
+
+php artisan make:migration add_votes_to_users_table --table=users
+
+
+- CREATE CONTROLLER (& resource)
+
+php artisan make:controller PhotoController --resource --model=Photo
+
+- CREATE FACTORY
+
+php artisan make:factory PostFactory --model=Post
+
+- CREATE SEEDER
+
+php artisan make:seeder UsersTableSeeder
+
+- CREATE TEST
+
+php artisan make:test UserTest --unit
+
+
+- RUN TEST
+
+./vendor/bin/phpunit
+
+./vendor/bin/phpunit --filter=xxx
+
+
