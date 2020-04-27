8bfc616f4181fe757d4fbb9e8dddd52f7433e99f
Jclavo <jclavotafur@gmail.com>
2020-04-27 20:27:45 -0300

    comment function to format date save in db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+use App\Database\Query\Grammars\MySqlGrammar;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +28,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        // DB::connection()->setQueryGrammar(new MySqlGrammar);
     }
 }
