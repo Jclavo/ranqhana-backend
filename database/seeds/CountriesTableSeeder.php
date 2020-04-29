289108018078e6bd85af44d33de13b70ce33ecd5
Jclavo <jclavotafur@gmail.com>
2020-04-29 20:58:40 -0300

    add timezone to country table

--- database/seeds/CountriesTableSeeder.php
+++ database/seeds/CountriesTableSeeder.php
@@ -18,7 +18,7 @@ class CountriesTableSeeder extends Seeder
         //         'id' => 1,
         //         'name' => 'Brazil',
         //     ]);
-        factory(Country::class)->create(['country_code' => '55','name' => 'Brazil']);
-        factory(Country::class)->create(['country_code' => '51','name' => 'Peru']);
+        factory(Country::class)->create(['country_code' => '55','name' => 'Brazil', 'timezone' => 'America/Sao_Paulo']);
+        factory(Country::class)->create(['country_code' => '51','name' => 'Peru', 'timezone' => 'America/Lima']);
     }
 }
