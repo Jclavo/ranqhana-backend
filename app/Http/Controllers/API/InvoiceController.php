06db87227294c9cbae5144ce05e443a1e17f63a1
Jclavo <jclavotafur@gmail.com>
2020-04-29 20:40:01 -0300

    convert the date to UTC to search in DB

--- app/Http/Controllers/API/InvoiceController.php
+++ app/Http/Controllers/API/InvoiceController.php
@@ -15,6 +15,7 @@ use App\Actions\BelongsToStore;
 
 use App\Http\Controllers\API\ItemController;
 use Carbon\Carbon;
+use App\Utils\CustomCarbon;
 
 class InvoiceController extends ResponseController
 {
@@ -126,27 +127,9 @@ class InvoiceController extends ResponseController
      * @param  \App\Invoice  $invoice
      * @return \Illuminate\Http\Response
      */
-    // public function destroy(int $id)
-    // {
-    //     $invoice = Invoice::find($id);
-
-    //     if (is_null($invoice)) {
-    //         return $this->sendError('Invoice not found.');
-    //     }
-
-    //     $belongs = User::where('id','=',Auth::user()->id)
-    //                     ->where('store_id','=',Auth::user()->store_id)
-    //                     ->get();
-        
-        
-    //     if(count($belongs) == 0) {
-    //         return $this->sendError('Invoice does not belongs to the logged user.');
-    //     }
-
-    //     $invoice->delete();
-
-    //     return $this->sendResponse($invoice->toArray(), 'Invoice Anulled/Canceled successfully.');
-    // }
+    public function destroy(int $id)
+    {
+    }
 
     /**
      * 
@@ -154,25 +137,28 @@ class InvoiceController extends ResponseController
 
     public function pagination(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            // 'subtotal' => 'required|numeric|gt:0',
+            // 'taxes' => 'numeric|gte:0|lt:subtotal',
+            // 'discount' => 'numeric|gte:0|lt:subtotal',
+            'type_id' => 'required|exists:invoice_types,id',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError($validator->errors()->first());
+        }
+
         $sortArray = array("asc", "ASC", "desc", "DESC");
 
-        $store_id = $request->store_id;
         // SearchOptions values
-        $per_page      = $request->searchOption['per_page'];
-        $sortColumn    = $request->searchOption['sortColumn'];
-        $sortDirection = $request->searchOption['sortDirection'];
-        $searchValue   = $request->searchOption['searchValue'];
-        $fromDate      = $request->searchOption['fromDate'];
-        $toDate        = $request->searchOption['toDate'];
-        $type_id       = $request->searchOption['type_id'];
+        $per_page      = $request->per_page;
+        $sortColumn    = $request->sortColumn;
+        $sortDirection = $request->sortDirection;
+        $searchValue   = $request->searchValue;
+        $fromDate      = $request->fromDate;
+        $toDate        = $request->toDate;
+        $type_id       = $request->type_id;
 
-        //change date format
-        $fromDate = Carbon::createFromFormat('Y-m-d H:i:s', $fromDate . ' 00:00:00', 'America/Sao_Paulo');
-        $fromDate->setTimezone('UTC');
-
-        $toDate = Carbon::createFromFormat('Y-m-d H:i:s', $toDate . ' 23:59:59', 'America/Sao_Paulo');
-        $toDate->setTimezone('UTC');
-        
         // Initialize values if they are empty.
         if (empty($per_page)) {
             $per_page = 20;
@@ -203,7 +189,11 @@ class InvoiceController extends ResponseController
         });
 
         $query->when((!empty($fromDate)) && (!empty($toDate)) , function ($q) use($fromDate,$toDate) {
-            // return $q->whereBetween('created_at', [$fromDate,$toDate]);
+
+            //change date format
+            $fromDate = CustomCarbon::UTCtoCountryTZ($fromDate,'00:00:00', 'America/Sao_Paulo');
+            $toDate = CustomCarbon::UTCtoCountryTZ($toDate,'23:59:59', 'America/Sao_Paulo');
+
             return $q->whereBetween('created_at',[ $fromDate." 00:00:00", $toDate." 23:59:59"]);
         });
 
