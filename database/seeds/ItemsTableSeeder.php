e481e08cf8171a38370381ad08dcb9b4065eca6d
Jclavo <jclavotafur@gmail.com>
2020-04-30 18:48:56 -0300

    add some custom factories

--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -13,6 +13,14 @@ class ItemsTableSeeder extends Seeder
     public function run()
     {
         factory(Item::class, 300)->create();
-        // factory(Item::class, 200)->create();
+        factory(Item::class)->create(
+            ['name' => 'pastillas', 'price' => 5, 'stock' => 200, 'stocked' => true, 'store_id' => 1 ]
+        );
+        factory(Item::class)->create(
+            ['name' => 'gaseosa', 'price' => 8, 'stock' => 500, 'stocked' => true, 'store_id' => 1 ]
+        );
+        factory(Item::class)->create(
+            ['name' => 'marmita', 'price' => 12, 'stock' => 0, 'stocked' => false, 'store_id' => 1 ]
+        );
     }
 }
