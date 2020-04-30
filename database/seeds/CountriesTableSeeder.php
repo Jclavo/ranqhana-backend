ac79fc88b7d08e2b8563c7f37871a1c912c10089
Jclavo <jclavotafur@gmail.com>
2020-04-30 18:48:31 -0300

    delete unnecesary code

--- database/seeds/CountriesTableSeeder.php
+++ database/seeds/CountriesTableSeeder.php
@@ -13,11 +13,6 @@ class CountriesTableSeeder extends Seeder
      */
     public function run()
     {
-        // DB::table('countries')->insert(
-        //     [
-        //         'id' => 1,
-        //         'name' => 'Brazil',
-        //     ]);
         factory(Country::class)->create(['country_code' => '55','name' => 'Brazil', 'timezone' => 'America/Sao_Paulo']);
         factory(Country::class)->create(['country_code' => '51','name' => 'Peru', 'timezone' => 'America/Lima']);
     }
