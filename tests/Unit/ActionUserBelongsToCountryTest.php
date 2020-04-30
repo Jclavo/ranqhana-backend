ef0ebf3a3f23f86b9fc372766fa2c8f7d6e4038a
Jclavo <jclavotafur@gmail.com>
2020-04-30 20:56:06 -0300

    test validate user belongs to a country

---
+++ tests/Unit/ActionUserBelongsToCountryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use App\Country;
+use App\Store;
+use App\Actions\User\UserBelongsToCountry;
+use Tests\TestCase;
+
+class ActionUserBelongsToCountryTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+    public function test_action_user_belongs_to_country_fail()
+    {
+        $user = factory(User::class)->create();
+
+        $action = new UserBelongsToCountry($user->identification, '55');
+
+        $this->assertFalse($action->passes());
+
+    }
+
+    public function test_action_user_belongs_to_country_ok()
+    {
+        $country = factory(Country::class)->create();
+        $store = factory(Store::class)->create(['country_id' => $country->id ]);
+        $user = factory(User::class)->create(['store_id' => $store->id]);
+
+        $action = new UserBelongsToCountry($user->identification, $country->code);
+
+        $this->assertTrue($action->passes());
+
+    }
+}
