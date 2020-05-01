5bad1d3ed9f79660ffcd59312dae50c6833f0687
Jclavo <jclavotafur@gmail.com>
2020-05-01 16:58:09 -0300

    add baseModel to item

--- app/Item.php
+++ app/Item.php
@@ -2,10 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use App\BaseModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Item extends Model
+class Item extends BaseModel
 {
     use SoftDeletes;
     /**
