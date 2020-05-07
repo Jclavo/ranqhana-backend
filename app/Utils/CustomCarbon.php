0ca130ed4f80056e0bd633be4c67fcb6388b0115
Jclavo <jclavotafur@gmail.com>
2020-05-06 21:20:00 -0300

    funct from get UTC parse date Y/M/D

--- app/Utils/CustomCarbon.php
+++ app/Utils/CustomCarbon.php
@@ -9,6 +9,8 @@ class CustomCarbon
 
     static function UTCtoCountryTZ($date, $hours = '00:00:00', $tz){
 
+        $date = substr($date,0,10); //Get only the date part YYYY/MM/DD
+
         $date = $date . ' ' . $hours;
         $date = Carbon::createFromFormat('Y-m-d H:i:s', $date, $tz);
         // $date = Carbon::createFromFormat('Y-m-d H:i:s', $date . ' 00:00:00', 'America/Sao_Paulo');
