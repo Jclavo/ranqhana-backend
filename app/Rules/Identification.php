b9ffb66be0f231ff0128206ccdad7dff079e99f9
Jclavo <jclavotafur@gmail.com>
2020-05-21 20:55:12 -0300

    create rule to validate identification by country

---
+++ app/Rules/Identification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+use App\Store;
+
+class Identification implements Rule
+{
+    protected $store_id;
+    protected $digitsCountry = 0;
+
+    public function __construct($store_id = 0)
+    {
+        $this->store_id = $store_id;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        //Get country code from store
+        $storeQueryResult = Store::
+                            select('countries.code')
+                            ->join('countries', 'countries.id', '=', 'stores.country_id')
+                            ->where('stores.id', '=', $this->store_id)
+                            ->first();
+
+        if($storeQueryResult == null) return false;
+
+        $countryCode = $storeQueryResult->code;
+        
+        switch ($countryCode) {
+            case "55":
+                if(strlen($value) != 11){
+                    $this->digitsCountry = 11;
+                    return false;
+                }
+                break;
+            case "51":
+                if(strlen($value) != 8){
+                    $this->digitsCountry = 8;
+                    return false;
+                }
+                break;
+            default:
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The identification must be ' . $this->digitsCountry . ' characters for your store country.';
+    }
+}
---
+++ tests/Unit/RuleIdentificationTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use App\Country;
+use App\Store;
+use App\Rules\Identification;
+use Tests\TestCase;
+use Faker;
+
+class RuleIdentificationTest extends TestCase
+{
+    protected $faker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+        $this->faker = Faker\Factory::create();
+    }
+
+    public function test_rule_identification_empty()
+    {
+        $user = factory(User::class)->create();
+        $action = new Identification();
+        $this->assertFalse($action->passes('identification',$user->identification));
+    }
+
+    public function test_rule_identification_br_fail()
+    {
+        $user = factory(User::class,'brazilian')->create(['identification' => $this->faker->regexify('[A-Za-z0-9]{10}')]);
+        $action = new Identification($user->store_id);
+        $this->assertFalse($action->passes('identification',$user->identification));
+
+    }
+
+    public function test_rule_identification_br_ok()
+    {
+        $user = factory(User::class,'brazilian')->create();
+        $action = new Identification($user->store_id);
+        $this->assertTrue($action->passes('identification',$user->identification));
+    }
+    
+}
