c6a5965d8d6131f880af7be6163898c372a8d4d6
Jclavo <jclavotafur@gmail.com>
2020-06-21 18:45:18 -0300

    LocalUsers table relationship with Items

--- app/Models/RanqhanaUser.php
+++ app/Models/RanqhanaUser.php
@@ -9,4 +9,13 @@ class RanqhanaUser extends Model
     protected $fillable = [
         'external_user_id', 'company_project_id'
     ];
+
+    /**
+     * Get the roles for the project
+    */
+    public function items()
+    {
+        return $this->hasMany('App\Models\Item')
+                    ->orderBy('name');
+    }
 }
