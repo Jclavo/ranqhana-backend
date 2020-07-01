da573f4496b99c26d5c9c674b2bc80b766ee0444
Jclavo <jclavotafur@gmail.com>
2020-06-30 21:10:53 -0300

    comment getTimeZone in UserUtils

--- app/Utils/UserUtils.php
+++ app/Utils/UserUtils.php
@@ -4,20 +4,20 @@ namespace App\Utils;
 
 class UserUtils
 {
-    static function getTimezone($user){
+    // static function getTimezone($user){
 
-        $timezone = $user::
-                    select('countries.timezone')
-                    ->join('stores', 'stores.id', '=', 'users.store_id')
-                    ->join('countries', 'countries.id', '=', 'stores.country_id')
-                    ->where('users.id', '=', $user->id)
-                    ->where('users.store_id', '=', $user->store_id)
-                    ->first();
-                    // ->pluck('countries.timezone');
+    //     $timezone = $user::
+    //                 select('countries.timezone')
+    //                 ->join('stores', 'stores.id', '=', 'users.store_id')
+    //                 ->join('countries', 'countries.id', '=', 'stores.country_id')
+    //                 ->where('users.id', '=', $user->id)
+    //                 ->where('users.store_id', '=', $user->store_id)
+    //                 ->first();
+    //                 // ->pluck('countries.timezone');
 
-        if($timezone == null) return 'UTC';
-        return $timezone->timezone;
-    }
+    //     if($timezone == null) return 'UTC';
+    //     return $timezone->timezone;
+    // }
 
     static function generateLogin($identification,$store_id){
         return $identification . $store_id;
