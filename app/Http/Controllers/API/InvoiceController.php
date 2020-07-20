7ca42692e25b6ab6a3320450408ece6d9d78aadb
Jclavo <jclavotafur@gmail.com>
2020-07-19 22:37:16 -0300

    delete report function from Invoice and its detail

--- app/Http/Controllers/API/InvoiceController.php
+++ app/Http/Controllers/API/InvoiceController.php
@@ -6,17 +6,16 @@ use App\Models\Invoice;
 use App\Models\InvoiceDetail;
 use App\Models\User;
 
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\ResponseController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
-use App\Actions\Invoice\InvoiceAnull;
-use App\Actions\BelongsToStore;
 use Illuminate\Validation\Rule; 
 
-use App\Http\Controllers\API\ItemController;
-use Carbon\Carbon;
+//Actions
+use App\Actions\Invoice\InvoiceAnull;
+use App\Actions\BelongsToStore;
 
 //Utils
 use App\Utils\PaginationUtils;
@@ -211,85 +210,6 @@ class InvoiceController extends ResponseController
         return $this->sendResponse($invoice->toArray(), 'Invoice Anulled successfully.');
     }
 
-
-    /**
-     * 
-     */
-    public function graphicReport(Request $request){
-
-        $validator = Validator::make($request->all(), [
-            'fromDate' => 'date',
-            'toDate' => 'date|after_or_equal:fromDate',
-            'type_id' => 'required|exists:invoice_types,id',
-            'searchBy' => [Rule::in(['Y', 'M', 'D', 'H'])]
-        ]);
-
-        if ($validator->fails()) {
-            return $this->sendError($validator->errors()->first());
-        }
-
-        $fromDate = $request->fromDate;
-        $toDate = $request->toDate;
-        $type_id = $request->type_id;
-        $searchBy = $request->searchBy;
-
-        $query = Invoice::query();
-
-        $query->select('invoices.total','invoices.created_at')
-              ->where('type_id', '=', $type_id);
-
-
-        $query->when((!empty($fromDate)) && (!empty($toDate)) , function ($q) use($fromDate,$toDate) {
-
-            $timezome = Auth::user()->getTimezone();
-            //change date format
-            $fromDate = CustomCarbon::UTCtoCountryTZ($fromDate,'00:00:00', $timezome);
-            $toDate = CustomCarbon::UTCtoCountryTZ($toDate,'23:59:59', $timezome);
-
-            return $q->whereBetween('created_at',[$fromDate, $toDate]);
-        });
-
-       
-        $rawQuery = $query->get()
-                    ->groupBy(function($date) use($searchBy){
-                        //Only in hours case 
-                        $timezome = Auth::user()->getTimezone();
-                        $date = Carbon::createFromFormat('Y-m-d H:i:s', $date->created_at, 'UTC');
-                        $date->setTimezone($timezome); 
-
-                        switch ($searchBy) {
-                            case 'Y':
-                                return Carbon::parse($date)->format('Y'); // grouping by year
-                                break;
-                            case 'M':
-                                return Carbon::parse($date)->format('M/Y'); // grouping by month 
-                                break;
-                            case 'D':
-                                return Carbon::parse($date)->format('M/D-d'); // grouping by day
-                                break;
-                            case 'H':
-                                return Carbon::parse($date)->format('D g A', $timezome); // grouping by day
-                                // format('g:i A');
-                                break;
-                            default:
-                                # code...
-                                break;
-                        }
-                    })
-                    ->map(function ($row) {
-                        return $row->sum('total');
-                    });
-        
-        // Logic to convert the group by in an array with X/Y
-        $query_keys = array_keys($rawQuery->toArray());
-        $results = array();
-        foreach($query_keys as $key) {
-            array_push($results,['X' => $key, 'Y' => $rawQuery[$key]]);
-        }
-                    
-        return $this->sendResponse($results, 'Invoices retrieved successfully.' );
-
-    }
 
 
     
--- app/Http/Controllers/API/InvoiceDetailController.php
+++ app/Http/Controllers/API/InvoiceDetailController.php
@@ -186,4 +186,5 @@ class InvoiceDetailController extends ResponseController
     {
         //
     }
+
 }
