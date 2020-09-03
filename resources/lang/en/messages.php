16470b0444ca33f851ace080d54af9c5a0df6427
Jclavo <jclavotafur@gmail.com>
2020-09-03 10:40:53 -0300

    create message for CRUD in EN/PT

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'create' => 'Record created successfully.',
+    'read' => 'Record retrieved successfully.',
+    'update' => 'Record updated successfully.',
+    'delete' => 'Record deleted successfully.',
+    'pagination' => 'Records retrieved successfully.',
+];
---
+++ resources/lang/pt/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'create' => 'Registro criado com éxito.',
+    'read' => 'Registro retornado com éxito',
+    'update' => 'Registro atualizado com éxito.',
+    'delete' => 'Registro apagado com éxito.',
+    'pagination' => 'Registros retornados com éxito',
+];
