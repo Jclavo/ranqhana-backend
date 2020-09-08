45c9c7b2119f6a1bc2b7e10ef73fbe455901bfaf
Jclavo <jclavotafur@gmail.com>
2020-09-08 20:06:32 -0300

    Create LanguageTrait to load translations 4 model

--- app/Traits/LanguageTrait.php
+++ app/Traits/LanguageTrait.php
@@ -6,46 +6,19 @@ use Illuminate\Database\Eloquent\Collection;
 
 trait LanguageTrait
 {
-    // public function generateSlug($string)
-    // {
-    //     return strtolower(preg_replace(
-    //         ['/[^\w\s]+/', '/\s+/'],
-    //         ['', '-'],
-    //         $string
-    //     ));
-    // }
-
+    /**
+     * Load translation values for model
+     */
     public function translate($collections){
 
-        $newQuery = array();
-        // $translations = $query->load('translations');
+        if (method_exists($collections, 'translations')) {
+            $collections->load('translations');
 
-        $collections->load('translations');
-
-        foreach ($collections->translations as $translation) {
-            $collections[$translation->key] = $translation->value;
+            foreach ($collections->translations as $translation) {
+                $collections[$translation->key] = $translation->value;
+            }
+            unset($collections['translations']);
         }
-        unset($collections['translations']);
-
-        // if (is_array($collections)) {
-            
-        //     foreach ($collection as $collection) {
-
-        //         foreach ($collection->translations as $translation) {
-        //             $collection[$translation->key] = $translation->value;
-        //         }
-        //         unset($collections['translations']);
-                
-        //     }
-
-        // }else{
-
-        //     foreach ($collections->translations as $translation) {
-        //         $collections[$translation->key] = $translation->value;
-        //     }
-        //     unset($collections['translations']);
-
-        // }
 
     }
 }
