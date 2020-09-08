c4b7749e37d8e5c03b4c191569116fcebdb036dd
Jclavo <jclavotafur@gmail.com>
2020-09-08 20:28:20 -0300

    add relationship InvoiceStage and TranslationModel

--- app/Models/InvoiceStages.php
+++ app/Models/InvoiceStages.php
@@ -2,11 +2,33 @@
 
 namespace App\Models;;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\BaseModel;
 
-class InvoiceStages extends Model
+use Illuminate\Support\Facades\App;
+
+class InvoiceStages extends BaseModel
 {
+
+    /** 
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    
     protected $fillable = [
         'code', 'description'
     ];
+
+     /**
+     * Relationships
+     */
+
+    /**
+     * Get all of the Invoice Stages' translations.
+     */
+    public function translations()
+    {
+        return $this->morphMany(Translation::class, 'translationable')
+                    ->where('locale',App::getLocale());
+    }
 }
