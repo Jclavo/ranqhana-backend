3a1ac6ff5c40fabd1d659654e01c5a4d3130d339
Jclavo <jclavotafur@gmail.com>
2020-10-02 10:12:28 -0300

    create scope to only get records from user company

---
+++ app/Scopes/Belongs2CompanyScope.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Auth;
+
+class Belongs2CompanyScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        //$builder->where('age', '>', 200);
+        $builder->whereIn('user_id', function($query){
+            $query->select('ranqhana_users.id')
+                ->from('ranqhana_users')
+                ->where('ranqhana_users.company_project_id', '=', Auth::user()->company_project_id);
+        });
+    }
+}
