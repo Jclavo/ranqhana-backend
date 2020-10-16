00ff0d7ff1628e76ea72c4bbf48b929a6266f86b
Jclavo <jclavotafur@gmail.com>
2020-10-15 22:28:34 -0300

    create function to convert to UTC and back

--- app/Utils/CustomCarbon.php
+++ app/Utils/CustomCarbon.php
@@ -3,18 +3,37 @@
 namespace App\Utils;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class CustomCarbon
 {
 
-    static function UTCtoCountryTZ($date, $hours = '00:00:00', $tz){
+    static function countryTZtoUTC($date, $hours = null, $tz = null){
 
-        $date = substr($date,0,10); //Get only the date part YYYY/MM/DD
+        empty($tz) ? $tz = Auth::user()->getTimezone() : null;
 
-        $date = $date . ' ' . $hours;
+        if(!empty($hours)){
+            $date = substr($date,0,10); //Get only the date part YYYY/MM/DD
+            $date = $date . ' ' . $hours;
+        }
+
         $date = Carbon::createFromFormat('Y-m-d H:i:s', $date, $tz);
         // $date = Carbon::createFromFormat('Y-m-d H:i:s', $date . ' 00:00:00', 'America/Sao_Paulo');
         $date->setTimezone('UTC');
+        return $date;
+    }
+
+    static function UTCtoCountryTZ($date, $hours = null, $tz = null){
+
+        empty($tz) ? $tz = Auth::user()->getTimezone() : null;
+
+        if(!empty($hours)){
+            $date = substr($date,0,10); //Get only the date part YYYY/MM/DD
+            $date = $date . ' ' . $hours;
+        }
+
+        $date = Carbon::createFromFormat('Y-m-d H:i:s', $date, 'UTC');
+        $date->setTimezone($tz);
         return $date;
     }
 
