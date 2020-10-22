0cc7a4e1e6f47daef3ed6a86eb103bb078787313
Jclavo <jclavotafur@gmail.com>
2020-10-22 09:42:33 -0300

    add static method to get stage in OrderStage Model

--- app/Models/OrderStage.php
+++ app/Models/OrderStage.php
@@ -40,8 +40,48 @@ class OrderStage extends BaseModel
         return 1;
     }
 
+    static function getForRequested()
+    {
+        return 2;
+    }
+
+    static function getForAccepted()
+    {
+        return 3;
+    }
+
+    static function getForPreparing()
+    {
+        return 4;
+    }
+
+    static function getForWrapped()
+    {
+        return 5;
+    }
+
+    static function getForReady()
+    {
+        return 6;
+    }
+
+    static function getForShipped()
+    {
+        return 7;
+    }
+
+    static function getForDelivered()
+    {
+        return 8;
+    }
+
     static function getForCancel()
     {
         return 9;
+    }
+
+    static function getForAutomatic()
+    {
+        return 10;
     }
 }
