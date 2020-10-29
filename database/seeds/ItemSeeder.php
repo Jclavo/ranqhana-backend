881dd5d5d1a8462b146587854a38109fbc2cb88d
Jclavo <jclavotafur@gmail.com>
2020-10-29 18:34:27 -0300

    modify seeder names according standards

---
+++ database/seeds/ItemSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Item;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // factory(Item::class, 100)->create();
+        // factory(Item::class)->create(
+        //     ['name' => 'pastillas', 'price' => 5, 'stock' => 200, 'stocked' => true]
+        // );
+        // factory(Item::class)->create(
+        //     ['name' => 'gaseosa', 'price' => 8, 'stock' => 500, 'stocked' => true ]
+        // );
+        // factory(Item::class)->create(
+        //     ['name' => 'marmita', 'price' => 12, 'stock' => 0, 'stocked' => false ]
+        // );
+    }
+}
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Seeder;
 
 use App\Models\User;
 
-class UserTableSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
