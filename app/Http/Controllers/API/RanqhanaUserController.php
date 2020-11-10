263179154af8459ea112c6a6acf89698829f755e
Jclavo <jclavotafur@gmail.com>
2020-11-10 20:47:57 -0300

    comment method store in RanqhanaUserController

--- app/Http/Controllers/API/RanqhanaUserController.php
+++ app/Http/Controllers/API/RanqhanaUserController.php
@@ -14,23 +14,23 @@ class RanqhanaUserController extends ResponseController{
 
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'external_user_id' => 'required|exists:mysql_roles.users,id',
-            'login' => 'required|exists:mysql_roles.users,login',
-            'company_project_id' => 'required|exists:mysql_roles.users,company_project_id',
-        ]);
+        // $validator = Validator::make($request->all(), [
+        //     'external_user_id' => 'required|exists:mysql_roles.users,id',
+        //     'login' => 'required|exists:mysql_roles.users,login',
+        //     'company_project_id' => 'required|exists:mysql_roles.users,company_project_id',
+        // ]);
         
-        if ($validator->fails()) {
-            return $this->sendError($validator->errors()->first());
-        }
+        // if ($validator->fails()) {
+        //     return $this->sendError($validator->errors()->first());
+        // }
         
-        $ranqhanaUser = RanqhanaUser::updateOrCreate([
-            'external_user_id' => $request->external_user_id,
-            'login' => $request->login,
-            'company_project_id' => $request->company_project_id,
-        ]);
+        // $ranqhanaUser = RanqhanaUser::updateOrCreate([
+        //     'external_user_id' => $request->external_user_id,
+        //     'login' => $request->login,
+        //     'company_project_id' => $request->company_project_id,
+        // ]);
 
-        return $this->sendResponse($ranqhanaUser->toArray(), 'Record syncronized!');  
+        // return $this->sendResponse($ranqhanaUser->toArray(), 'Record syncronized!');  
     }
 
 }
